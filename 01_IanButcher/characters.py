a0e7425cd5619841c44434a75d25339cfe1f365a
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-03-21 14:15:07 -0300

    update

--- 01_IanButcher/characters.py
+++ 01_IanButcher/characters.py
@@ -3,7 +3,10 @@
 # Importar math random para criticos
 import random
 
-
+#comentario clase
+lineas = '''
+---------------------------------------------------------------
+'''
 
 # Crear clase
 class Character:
@@ -13,47 +16,65 @@ class Character:
         self.damage = damage
         self.maxHealthPoints = maxHealtPoints
         self.actualHealthPoints = maxHealtPoints
+        self.boostDamage = False
     
     # Esta vivo?
     # def is_alive(self):
     #    return (self.actualHealthPoints > 0)
 
-    # Funcion random crits
-    def crits():
-        randomCrit = random.randint(0, 4)
-        return (randomCrit)
-
     # Metodo atacar 
-    def atack(self):
-        # Ver si es critico o no
-        self.crits()
-        if self.randomCrit == 1 and self.actualHealthPoints != 0:
+    def atack(self, damage):
+
+        # Ver si es critico
+        randomCrit = random.randint(0, 4)
+        if randomCrit == 1 and self.actualHealthPoints != 0:
             # El daño es %50 mas alto
-            critHit = self.damage * 1.5
+            critHit = damage * 1.5
+            print(lineas)
             print(f"Golpe critico a {self.name}!")
-            # Quitar vida
+            
+            #Quitar vida
             self.actualHealthPoints -= critHit
 
         #Golpe normal (No critico)
-        elif self.randomCrit != 1 and self.actualHealthPoints != 0:
-            print(f"Haz atacado a {self.name}")
-            self.actualHealthPoints -= self.damage
+        elif randomCrit != 1 and self.actualHealthPoints != 0:
+            print(lineas)
+            print(f"Has atacado a {self.name}")
+            
+            # Quitar vida
+            self.actualHealthPoints -= damage
 
         # Volver la vida a 0
         elif self.actualHealthPoints <= 0:
             self.actualHealthPoints = 0
             print(f"{self.name} ha sido derrotado! GG WP")
 
+        
+
         print(f"{self.name} ahora tiene {self.actualHealthPoints}")
+        print(lineas)
 
     # Metodo curar
-    def heal(self, actualHealthPoints, maxHealthPoints):
+    def heal(self):
 
-        if actualHealthPoints != 0:
-            self.actualHealthPoints += random.randint(10, 30)
+        if self.actualHealthPoints != 0:
+            healed = random.randint(10, 30)
+            self.actualHealthPoints += healed
         else:
             print("El personaje murio, no puedes curarlo!")
         
-        if actualHealthPoints > maxHealthPoints:
-            actualHealthPoints = maxHealthPoints
-        print (f"{self.name} Se has curado {actualHealthPoints}!")
+        if self.actualHealthPoints > self.maxHealthPoints:
+            self.actualHealthPoints = self.maxHealthPoints
+        
+        print(lineas)
+        print (f"{self.name} Se ha curado {healed}! Ahora tiene {self.actualHealthPoints}")
+        print(lineas)
+
+    # Metodo de escudo y defensa
+    #boostDamageUses = 10
+    # def boost(self, enemy):
+    #    if self.boostDamageUses > 0:
+     #       self.boostDamageUses -= 1
+      #      enemy.self.boostDamage = True
+       # else:
+        #    print("Tu escudo esta roto, no puedes defenderte mas :c")
--- 01_IanButcher/main.py
+++ 01_IanButcher/main.py
@@ -4,21 +4,30 @@ from characters import Character
 player1 = Character(input("Ingrese el nombre del personaje: "), 10, 150)
 player2 = Character(input("Ingrese el nombre del personaje: "), 10, 150)
 
+#players = [player1, player2]
+
 while player1.actualHealthPoints != 0 and player2.actualHealthPoints != 0:
     # Turnos 
-    turno1 = int(input(f"Turno de {player1.name}, ingresa 1 para atacar y 2 para curarte!"))
+    # idea de hacerlos con for no me saliofor player in players:
+    
+    turno1 = int(input(f"Turno de {player1.name}, ingresa 1 para atacar, 2 para curarte y 3 para defenderte del proximo ataque!"))
     if turno1 == 1:
-        player2.atack()
+        player2.atack(15)
     elif turno1 == 2:
-        player1.heal()   
+        player1.heal() 
+    elif turno1 == 3:
+        player1.shield(player2)  
      
-    turno2 = input(f"Turno de {player2.name}")
+    turno2 = int(input(f"Turno de {player2.name} ingresa 1 para atacar, 2 para curarte y 3 para defenderte del proximo ataque!"))
     if turno2 == 1:
-        player1.atack()
+        player1.atack(15)
     elif turno2 == 2:
         player2.heal()
+    elif turno1 == 3:
+        player2.shield(player1) 
 
-    if player1.actualHealthPoints <= 0 or player1.actualHealthPoints <= 0:
+    if player1.actualHealthPoints <= 0 or player2.actualHealthPoints <= 0:
         print ("Juego finalizado uwu")
         break
 
+ 
